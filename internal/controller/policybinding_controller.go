a444dce09bbf9fe0abd545629c5160c32a3b3e73
Scot Wells <wells.scot@gmail.com>
2025-05-28 16:52:02 -0500

    chore: remove license header

--- internal/controller/policybinding_controller.go
+++ internal/controller/policybinding_controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 // Package controller contains the PolicyBinding controller and its associated logic
 // for reconciling PolicyBinding custom resources. It ensures that the desired state,
 // as defined by PolicyBinding objects, is reflected in the OpenFGA authorization system.
